d839b984bc2971b95313719d9b29bd41d8b8046c
penajc <jhon.pena@hotmail.com>
2022-11-24 16:19:29 -0500

    ensayo despliegue vercel

--- src/controllers/producto.controller.ts
+++ src/controllers/producto.controller.ts
@@ -1,21 +1,16 @@
+import {authenticate} from '@loopback/authentication';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
 import {Producto} from '../models';
 import {ProductoRepository} from '../repositories';
@@ -47,6 +42,7 @@ export class ProductoController {
     return this.productoRepository.create(producto);
   }
 
+  @authenticate.skip()
   @get('/productos/count')
   @response(200, {
     description: 'Producto model count',
@@ -58,6 +54,7 @@ export class ProductoController {
     return this.productoRepository.count(where);
   }
 
+  @authenticate.skip()
   @get('/productos')
   @response(200, {
     description: 'Array of Producto model instances',
@@ -95,6 +92,7 @@ export class ProductoController {
     return this.productoRepository.updateAll(producto, where);
   }
 
+  @authenticate.skip()
   @get('/productos/{id}')
   @response(200, {
     description: 'Producto model instance',
